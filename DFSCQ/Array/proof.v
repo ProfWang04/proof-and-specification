aa270c84e2a64861ba58aa7798d3b6567d73177d
ProfWang04 <143494068+ProfWang04@users.noreply.github.com>
2025-07-03 20:40:55 +0800

    Update proof.v

--- DFSCQ/Array/proof.v
+++ DFSCQ/Array/proof.v
@@ -1,1 +1,217 @@
+Require Import Mem.
+Require Import ListUtils.
+Require Import List Omega Ring Word Pred PredCrash.
+Require Import Prog Hoare SepAuto BasicProg.
+Require Import FunctionalExtensionality.
+Require Import WordAuto.
+Require Import AsyncDisk.
+
+Import ListNotations.
+
+Set Implicit Arguments.
+Set Default Proof Using "Type".
+
+
+(** * A generic array predicate: a sequence of consecutive points-to facts *)
+
+Section GenArray.
+
+  Variable V VP : Type.
+  Variable pts : addr -> V -> @pred addr addr_eq_dec VP.
+
+  Infix "|-?->" := pts (at level 35) : pred_scope.
+
+  Fixpoint arrayN (a : addr) (vs : list V) : @pred _ addr_eq_dec _ :=
+    match vs with
+      | nil => emp
+      | v :: vs' => a |-?-> v * arrayN (S a) vs'
+    end%pred.
+
+  Lemma arrayN_unify : forall (a b : list V) s,
+    a = b -> arrayN s a =p=> arrayN s b.
+  Proof.
+    intros; subst; auto.
+  Qed.
+
+  Lemma arrayN_one: forall v,
+      0 |-?-> v <=p=> arrayN 0 [v].
+  Proof.
+    split; cancel.
+  Qed.
+
+  Lemma isolateN_fwd' : forall vs i a (default : V),
+    i < length vs
+    -> arrayN a vs =p=> arrayN a (firstn i vs)
+       * (a + i) |-?-> selN vs i default
+       * arrayN (a + i + 1) (skipn (S i) vs).
+  Proof.
+    induction vs; simpl; intuition.
+
+    inversion H.
+
+    destruct i; simpl.
+
+    replace (a0 + 0) with (a0) by omega.
+    replace (a0 + 1) with (S a0) by omega.
+    cancel.
+
+    eapply pimpl_trans; [ apply pimpl_sep_star; [ apply pimpl_refl | apply IHvs ] | ]; clear IHvs.
+    instantiate (1 := i); omega.
+    simpl.
+    replace (S (a0 + i)) with (a0 + S i) by omega.
+    replace (S (a0 + i + 1)) with (a0 + S i + 1) by omega.
+    cancel.
+  Qed.
+
+  Theorem isolateN_fwd : forall (default : V) a i vs,
+    i < length vs
+    -> arrayN a vs =p=> arrayN a (firstn i vs)
+       * (a + i) |-?-> selN vs i default
+       * arrayN (a + i + 1) (skipn (S i) vs).
+  Proof.
+    intros.
+    eapply pimpl_trans; [ apply isolateN_fwd' | ].
+    eassumption.
+    apply pimpl_refl.
+  Qed.
+
+  Lemma isolateN_bwd' : forall vs i a (default : V),
+    i < length vs
+    -> arrayN a (firstn i vs)
+       * (a + i) |-?-> selN vs i default
+       * arrayN (a + i + 1) (skipn (S i) vs)
+    =p=> arrayN a vs.
+  Proof.
+    induction vs; simpl; intuition.
+
+    inversion H.
+
+    destruct i; simpl.
+
+    replace (a0 + 0) with (a0) by omega.
+    replace (a0 + 1) with (S a0) by omega.
+    cancel.
+
+    eapply pimpl_trans; [ | apply pimpl_sep_star; [ apply pimpl_refl | apply IHvs ] ]; clear IHvs.
+    2: instantiate (1 := i); omega.
+    simpl.
+    replace (a0 + S i) with (S (a0 + i)) by omega.
+    cancel.
+  Qed.
+
+  Theorem isolateN_bwd : forall (default : V) a i vs,
+    i < length vs
+    -> arrayN a (firstn i vs)
+       * (a + i) |-?-> selN vs i default
+       * arrayN (a + i + 1) (skipn (S i) vs)
+    =p=> arrayN a vs.
+  Proof.
+    intros.
+    eapply pimpl_trans; [ | apply isolateN_bwd' ].
+    2: eassumption.
+    apply pimpl_refl.
+  Qed.
+
+  Theorem arrayN_isolate : forall (default : V) a i vs,
+    i < length vs
+    -> arrayN a vs <=p=>
+       arrayN a (firstn i vs)
+       * (a + i) |-?-> selN vs i default
+       * arrayN (a + i + 1) (skipn (S i) vs).
+  Proof.
+    unfold piff; split.
+    apply isolateN_fwd; auto.
+    apply isolateN_bwd; auto.
+  Qed.
+
+  Theorem isolate_fwd_upd : forall (v : V) a i vs,
+    i < length vs
+    -> arrayN a (updN vs i v) <=p=>
+       arrayN a (firstn i vs)
+       * (a + i) |-?-> v
+       * arrayN (a + i + 1) (skipn (S i) vs).
+  Proof.
+    intros.
+    erewrite arrayN_isolate with (vs:=updN vs i v) (i:=i) (default:=v);
+      autorewrite with lists; auto.
+    unfold piff; split.
+    cancel; autorewrite with lists; cancel.
+    cancel; autorewrite with lists; cancel.
+  Qed.
+
+  Theorem isolateN_bwd_upd : forall (v : V) a i vs,
+    i < length vs
+    -> arrayN a (firstn i vs)
+       * (a + i) |-?-> v
+       * arrayN (a + i + 1) (skipn (S i) vs)
+       =p=> arrayN a (updN vs i v).
+  Proof.
+    intros.
+    erewrite <- isolateN_bwd with (vs:=updN vs i v) (i:=i) (default:=v).
+    rewrite selN_updN_eq by auto.
+    rewrite firstn_updN_oob by auto.
+    rewrite skipN_updN' by auto.
+    cancel.
+    rewrite length_updN.
+    auto.
+  Qed.
+
+  Theorem arrayN_app : forall (a b : list V) st,
+    arrayN st (a ++ b) <=p=>
+    arrayN st a * arrayN (st + length a) b.
+  Proof.
+    induction a; split; simpl; auto.
+    rewrite Nat.add_0_r; cancel.
+    rewrite Nat.add_0_r; cancel.
+    rewrite IHa.
+    replace (S st + length a0) with (st + S (length a0)) by omega.
+    cancel.
+    rewrite IHa.
+    replace (S st + length a0) with (st + S (length a0)) by omega.
+    cancel.
+  Qed.
+
+  Theorem arrayN_split : forall i (a : list V) st,
+    arrayN st a <=p=>
+    arrayN st (firstn i a) * arrayN (st + i) (skipn i a).
+  Proof.
+    intros.
+    destruct (lt_dec i (length a)).
+    erewrite arrayN_isolate; eauto.
+    setoid_rewrite arrayN_isolate with (i := 0) at 4.
+    rewrite skipn_skipn, skipn_selN.
+    replace (st + i + 0) with (st+i) by omega.
+    replace (1 + i) with (S i) by omega.
+    replace (i + 0) with i by omega.
+    split; cancel.
+    rewrite skipn_length.
+    omega.
+    rewrite firstn_oob, skipn_oob by omega.
+    split; cancel.
+    Grab Existential Variables.
+    destruct a.
+    contradict l; simpl; omega.
+    exact v.
+  Qed.
+
+
+  Lemma arrayN_ptsto_selN_0 : forall l (def : V) a,
+    length l = 1 ->
+    arrayN a l <=p=> (a |-?-> selN l 0 def)%pred.
+  Proof.
+    destruct l; simpl; intros; try congruence.
+    assert (length l = 0) by omega.
+    apply length_nil in H0; subst; simpl; split; cancel.
+  Qed.
+
+  Lemma arrayN_isolate_hd : forall l (def : V) a,
+    length l >= 1 ->
+    arrayN a l <=p=> (a |-?-> selN l 0 def * arrayN (a + 1) (skipn 1 l) )%pred.
+  Proof.
+    destruct l; simpl; intros; try omega.
+    replace (a + 1) with (S a) by omega; auto.
+  Qed.
+
+
+End GenArray.
 
