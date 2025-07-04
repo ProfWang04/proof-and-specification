a9ec6b810b9ee6aa4ef67d3d082c70a0d3c825f4
ProfWang04 <143494068+ProfWang04@users.noreply.github.com>
2025-07-04 10:17:34 +0800

    Update proof.v

--- DFSCQ/AsyncDisk/proof.v
+++ DFSCQ/AsyncDisk/proof.v
@@ -44,3 +44,43 @@ Proof.
   unfold upd_hashmap'.
   apply upd_hashmap_eq; auto.
 Qed.
+
+ (* Converting between hash and valu.*)
+Lemma hashlen_valulen: hashlen + (valulen - hashlen) = valulen.
+Proof.
+  rewrite valulen_is; auto.
+Qed.
+
+ Lemma hash_to_valu_inj : forall a b,
+  hash_to_valu a = hash_to_valu b ->
+  a = b.
+  unfold hash_to_valu.
+  unfold eq_rec_r, eq_rec.
+  rewrite <- hashlen_valulen.
+  intros.
+  rewrite <- (eq_rect_eq_dec eq_nat_dec) in H.
+  rewrite <- (eq_rect_eq_dec eq_nat_dec) in H.
+  unfold zext in *.
+  apply combine_inj in H.
+  intuition.
+Qed.
+ 
+Lemma sync_addr_ne : forall AT AEQ (m : @mem AT AEQ valuset) a a',
+  a <> a' ->
+  (sync_addr m a) a' = m a'.
+Proof.
+  unfold sync_addr; intros.
+  destruct (AEQ a a'); try congruence.
+Qed.
+
+Lemma sync_addr_eq : forall AT AEQ (m : @mem AT AEQ valuset) a a' vs,
+  a = a' ->
+  m a' = Some vs ->
+  (sync_addr m a) a' = Some (fst vs, nil).
+Proof.
+  unfold sync_addr; intros; subst.
+  destruct (AEQ a' a'); try congruence.
+  destruct (m a'); try congruence.
+  inversion H0; subst.
+  destruct vs; auto.
+Qed.
