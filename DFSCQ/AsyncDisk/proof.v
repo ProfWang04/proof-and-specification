d9b8fcfd2dd44740e7af437fde61d5d3a4579629
ProfWang04 <143494068+ProfWang04@users.noreply.github.com>
2025-07-04 10:15:53 +0800

    Update proof.v

--- DFSCQ/AsyncDisk/proof.v
+++ DFSCQ/AsyncDisk/proof.v
@@ -24,3 +24,23 @@ Proof.
   rewrite valulen_is. apply Nat.ltb_lt. compute; reflexivity.
   apply pow2_le_S.
 Qed.
+
+
+ Lemma upd_hashmap_eq : forall hm h k,
+  h <> default_hash ->
+  hashmap_get (upd_hashmap hm h k) h = Some k.
+Proof.
+  intros.
+  unfold hashmap_get.
+  destruct (weq h default_hash);
+  destruct (weq h h); intuition.
+Qed.
+
+Lemma upd_hashmap'_eq : forall hm h sz k,
+  h <> default_hash ->
+  hashmap_get (upd_hashmap' hm h k) h = Some (existT _ sz k).
+Proof.
+  intros.
+  unfold upd_hashmap'.
+  apply upd_hashmap_eq; auto.
+Qed.
