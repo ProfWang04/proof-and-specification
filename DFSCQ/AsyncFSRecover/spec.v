b336c3010efe7ff000cdafad6b10c5f2feb81169
ProfWang04 <143494068+ProfWang04@users.noreply.github.com>
2025-07-04 10:31:24 +0800

    Update spec.v

--- DFSCQ/AsyncFSRecover/spec.v
+++ DFSCQ/AsyncFSRecover/spec.v
@@ -1,1 +1,49 @@
 
+Require Import Prog.
+Require Import Log.
+Require Import BFile.
+Require Import Word.
+Require Import Omega.
+Require Import BasicProg.
+Require Import Bool.
+Require Import Pred PredCrash.
+Require Import DirName.
+Require Import Hoare.
+Require Import GenSepN.
+Require Import ListPred.
+Require Import SepAuto.
+Require Import Idempotent.
+Require Import Inode.
+Require Import List ListUtils.
+Require Import Balloc.
+Require Import Bytes.
+Require Import DirTree.
+Require Import Rec.
+Require Import Arith.
+Require Import Array.
+Require Import FSLayout.
+Require Import Cache.
+Require Import Errno.
+Require Import AsyncDisk.
+Require Import GroupLog.
+Require Import DiskLogHash.
+Require Import SuperBlock.
+Require Import DiskSet.
+Require Import AsyncFS.
+
+Set Implicit Arguments.
+Import ListNotations.
+
+
+Module AFS_RECOVER.
+
+  Import AFS.
+  Import DirTree.
+  Import DirTreeDef.
+
+  Parameter cachesize : nat.
+  Axiom cachesize_ok : cachesize <> 0.
+  Hint Resolve cachesize_ok.
+
+  Notation MSLL := BFILE.MSLL.
+  Notation MSAlloc := BFILE.MSAlloc.
